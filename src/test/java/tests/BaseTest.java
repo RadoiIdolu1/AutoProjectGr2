97e7c4da12986284ccbf3f61dfaa747fe699075f
RaduMihai <mrtwin999@yahoo.com>
2024-02-20 18:37:54 +0200

    Message

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import pages.BasePage;
+import pages.SignInPage.SignInPage;
+
+public class BaseTest {
+
+    public SignInPage signInPage = SignInPage.getInstance();
+
+    @BeforeSuite
+    public void init(){
+        BasePage.setUp();
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        BasePage.close();
+    }
+
+}
